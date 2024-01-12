2bd3c3dd68c6423153c10b270069c2275871c796
Mustafa Al-Shebeeb <mustafaalshebeeb@gmail.com>
2024-01-12 00:07:21 -0500

    Created the common buttons for a browser.

--- ChronoExplorer.py
+++ ChronoExplorer.py
@@ -24,6 +24,35 @@ class MainWindow(QMainWindow):
         toolbar = QToolBar()
         self.addToolBar(toolbar)
 
+        # Back button
+        back_btn = QAction('⮜', self) # back button that takes you to the last state
+        back_btn.triggered.connect(lambda: self.current_browser().back())
+        toolbar.addAction(back_btn)
+
+        # Forward button
+        forward_btn = QAction('⮞', self) # creates a forward button and if pressed then goes to next state
+        forward_btn.triggered.connect(lambda: self.current_browser().foward())
+        toolbar.addAction(forward_btn)
+
+        # Reload button
+        reload_btn = QAction('⟳', self) # Creates a reload button that resets the states
+        reload_btn.triggered.connect(lambda: self.current_browser().reload())
+        toolbar.addAction(reload_btn)
+
+        # Home BUtton
+        home_btn = QAction('⌂', self)
+        home_btn.triggered.connect(self.navigate_home)
+        toolbar.addAction(home_btn)
+
+
+
+
+
+
+
+
+
+
 
 
 
